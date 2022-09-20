757083e79f5672b4ba72810451831be8ff77a0e3
MartelAxe <martelaxe@gmail.com>
2022-09-19 20:10:05 -0500

    Finalizing contract adding some basic tests

--- contracts/Universe.sol
+++ contracts/Universe.sol
@@ -3,26 +3,29 @@ pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Roles.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Universe is Ownable {
-    using Roles for Roles.Role;
+contract Universe is Ownable, AccessControl {
     // Basic structure of Project, we will first define here mostly who can acess/ modify the project
     // Other elements of project TBD
     struct Project {
         uint256 projectId;
-        Roles.Role readyOnlyAccess;
-        Roles.Role fullAccess;
+        bytes32 READ_ONLY_ACCESS_ROLE;
+        bytes32 FULL_ACCESS_ROLE;
     }
 
-    uint256 projectIdCounter;
+    uint256 public projectIdCounter;
 
     mapping(uint256 => Project) public projects;
 
     // Events
 
-    event ProjectCreated(uint256 projectId, address[] readyOnlyAccess);
+    event ProjectCreated(
+        uint256 projectId,
+        address[] readyOnlyAccess,
+        address[] fullAccess
+    );
     event ProjectDeleted(uint256 projectId);
 
     //TBD if everyone can create a Project, if it costs something and if you mint something when you create it
@@ -31,77 +34,107 @@ contract Universe is Ownable {
         address[] memory readOnlyAccess,
         address[] memory fullAccess
     ) external {
-        uint256 projectId = projectId++;
+        uint256 projectId = projectIdCounter++;
 
         Project storage newProject = projects[projectId];
 
+        // We will concatenate the projectId to have the identifier for this project only
+
+        //Defining role readOnlyAccess role for this project Id
+
+        newProject.READ_ONLY_ACCESS_ROLE = keccak256(
+            abi.encodePacked("READ_ONLY_ACCESS_ROLE", toString(projectId))
+        );
+
+        //Defining role fullAccess role  for this project Id
+        newProject.FULL_ACCESS_ROLE = keccak256(
+            abi.encodePacked("READ_ONLY_ACCESS_ROLE", toString(projectId))
+        );
+
         //Starting addresses that can access/modify the project
         for (uint256 i = 0; i < readOnlyAccess.length; ++i) {
-            newProject.readOnlyAccess.add(readOnlyAccess[i]);
+            _setupRole(newProject.READ_ONLY_ACCESS_ROLE, readOnlyAccess[i]);
         }
 
         for (uint256 i = 0; i < fullAccess.length; ++i) {
-            newProject.fullAccess.add(fullAccess[i]);
+            _setupRole(newProject.FULL_ACCESS_ROLE, fullAccess[i]);
         }
 
         //The sender address that creates the project has full control of the project
-        newProject.fullAccess.add(msg.sender);
+        _setupRole(newProject.FULL_ACCESS_ROLE, msg.sender);
 
         emit ProjectCreated(projectId, readOnlyAccess, fullAccess);
     }
 
-    function deleteProject(uint256 projectId) onlyFullAccess(projectId) {
+    function deleteProject(uint256 projectId)
+        external
+        onlyRole(projects[projectId].FULL_ACCESS_ROLE)
+    {
         delete projects[projectId];
 
         emit ProjectDeleted(projectId);
     }
 
-    modifier onlyFullAccess(uint256 projectId) {
-        require(
-            projects[projectId].fullAccess.has(msg.sender),
-            "DOES_NOT_HAVE_FULL_ACCESS_TO_PROJECT"
-        );
-        _;
-    }
-
     function addFullAccessUser(uint256 projectId, address[] memory newAddress)
-        onlyFullAccess(projectId)
+        external
+        onlyRole(projects[projectId].FULL_ACCESS_ROLE)
     {
         Project storage project = projects[projectId];
 
         for (uint256 i = 0; i < newAddress.length; ++i) {
-            project.fullAccess.add(newAddress[i]);
+            _setupRole(project.FULL_ACCESS_ROLE, newAddress[i]);
         }
     }
 
     function removeFullAccess(uint256 projectId, address[] memory removeAddress)
-        onlyFullAccess(projectId)
+        external
+        onlyRole(projects[projectId].FULL_ACCESS_ROLE)
     {
         Project storage project = projects[projectId];
 
-        for (uint256 i = 0; i < newAddress.length; ++i) {
-            project.fullAccess.remove(newAddress[i]);
+        for (uint256 i = 0; i < removeAddress.length; ++i) {
+            _revokeRole(project.FULL_ACCESS_ROLE, removeAddress[i]);
         }
     }
 
     function addReadOnlyAccess(uint256 projectId, address[] memory newAddress)
-        onlyFullAccess(projectId)
+        external
+        onlyRole(projects[projectId].FULL_ACCESS_ROLE)
     {
         Project storage project = projects[projectId];
 
         for (uint256 i = 0; i < newAddress.length; ++i) {
-            project.readyOnlyAccess.add(newAddress[i]);
+            _setupRole(project.FULL_ACCESS_ROLE, newAddress[i]);
         }
     }
 
     function removeReadOnlyAccess(
         uint256 projectId,
         address[] memory removeAddress
-    ) onlyFullAccess(projectId) {
+    ) external onlyRole(projects[projectId].FULL_ACCESS_ROLE) {
         Project storage project = projects[projectId];
 
-        for (uint256 i = 0; i < newAddress.length; ++i) {
-            project.readyOnlyAccess.remove(newAddress[i]);
+        for (uint256 i = 0; i < removeAddress.length; ++i) {
+            _revokeRole(project.FULL_ACCESS_ROLE, removeAddress[i]);
         }
+    }
+
+    function toString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
--- test/Universe.ts
+++ test/Universe.ts
@@ -1,16 +1,82 @@
+import { Provider } from "@ethersproject/providers";
 import { expect } from "chai";
+import { Signer } from "ethers";
 import { ethers } from "hardhat";
+import { Universe } from "../typechain-types";
+import { PromiseOrValue } from "../typechain-types/common";
 
-describe("Universe", function () {
+describe("Universe Tests", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
 
+  let deployer;
+  let user2: { signer: string | Signer | Provider; address: PromiseOrValue<string>; };
+  let user3: { signer: string | Signer | Provider; address: PromiseOrValue<string> };
+  let user4: { address: PromiseOrValue<string>; };
+  let user5: { address: PromiseOrValue<string>; };
+
+  let universeContract: Universe;
+  before(async function () {
+    const getAccounts = async function () {
+      const accounts = [];
+      let signers = [];
+      signers = await ethers.getSigners();
+      for (const signer of signers) {
+        // eslint-disable-next-line no-await-in-loop
+        accounts.push({ signer, address: await signer.getAddress() });
+      } // populates the accounts array with addresses.
+      return accounts;
+    };
+
+    // REFACTOR
+    [deployer, user2, user3, user4, user5] = await getAccounts();
+  });
+
+  before((done) => {
+    setTimeout(done, 2000);
+  });
+
+
   describe("Deployment", function () {
     it("Should deploy correctly", async function () {
-      const UniverseContract = await ethers.getContractFactory("Universe");
-      const universeContract = await UniverseContract.deploy();
+      const universeFactory = await ethers.getContractFactory("Universe");
+      universeContract = await universeFactory.deploy();
       expect(universeContract).not.to.be.reverted;
+    });
+
+    it("Should create a new project", async function () {
+
+      const tx1 = await universeContract.createProject([], []);
+      await tx1.wait();
+      expect(await universeContract.projectIdCounter()).to.equal(1);
+
+
+
+    });
+
+    it("Should be able to add user to fullaccess role", async function () {
+
+      const tx1 = await universeContract.addFullAccessUser(0, [user2.address]);
+      await tx1.wait();
+
+
+    });
+
+    it("User without fullaccess role should not be able to give access", async function () {
+      await expect(universeContract.connect(user3.signer).addFullAccessUser(0, [user3.address])).to.be.reverted;
+
+
+    });
+
+    it("User with access in project 0 should not have access in project 1", async function () {
+
+      const tx1 = await universeContract.createProject([user3.address], [user4.address, user5.address]);
+      await tx1.wait();
+      await expect(universeContract.connect(user2.signer).addFullAccessUser(1, [user3.address])).to.be.reverted;
+
+      await expect(universeContract.connect(user2.signer).addFullAccessUser(0, [user3.address])).not.to.be.reverted;
+
     });
   });
 });
